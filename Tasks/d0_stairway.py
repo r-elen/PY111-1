9afeec769cf6ea3755882729577407e8d08cdbf9
r-elen <elen.ryazanova@gmail.com>
2022-09-15 20:30:41 +0300

    Сделано d0 (reverse)

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -10,7 +10,7 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     """
 
     print(stairway)
-    return direct_stairway_path(stairway)
+    return reverse_stairway_path(stairway)
 
 
 def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
@@ -24,5 +24,24 @@ def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, 
 
     for i in range(2, count_stairs):
         total_cost[i] = stairway[i] + min(total_cost[i-1], total_cost[i-2])
+
+    return total_cost[-1]
+
+
+def reverse_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    # stairway - цена ступени
+    count_stairs = len(stairway)  # кол-во ступеней
+    total_cost: list[int] = [float("inf")] * count_stairs  # стоимость дойти до ступеней
+
+    # начальные условия
+    total_cost[0] = stairway[0]  # для 1ой ступени
+    total_cost[1] = stairway[1]  # для 2ой ступени
+
+    #  обратный метод
+    for i in range(0, count_stairs):
+        if i + 1 < count_stairs:
+            total_cost[i + 1] = min(total_cost[i+1], total_cost[i] + stairway[i+1])
+        if i + 2 < count_stairs:
+            total_cost[i + 2] = min(total_cost[i+2], total_cost[i] + stairway[i+2])
 
     return total_cost[-1]
