097a4f439de7f36339d1b96a677092fe0b668954
r-elen <elen.ryazanova@gmail.com>
2022-09-15 19:25:22 +0300

    Сделано d0

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -18,3 +18,24 @@ def fib_iterative(n: int) -> int:
     """
     print(n)
     return 0
+
+
+def generator_fib(n):
+    if n < 0:
+        raise ValueError
+
+    a = 0
+    yield a
+
+    b = 1
+    yield b
+
+    for _ in range(1, n):
+        a, b = b, a+b
+        yield b
+
+
+N = 10
+
+list_fin_iterative = [fib_iterative(i) for i in range(N)]
+list_generator = [num for num in generator_fib(N-1)]
--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,21 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
+
     print(stairway)
-    return 0
+    return direct_stairway_path(stairway)
+
+
+def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    # stairway - цена ступени
+    count_stairs = len(stairway)  # кол-во ступеней
+    total_cost: list[int] = [0] * count_stairs  # стоимость дойти до ступеней
+
+    # начальные условия
+    total_cost[0] = stairway[0]  # для 1ой ступени
+    total_cost[1] = stairway[1]  # для 2ой ступени
+
+    for i in range(2, count_stairs):
+        total_cost[i] = stairway[i] + min(total_cost[i-1], total_cost[i-2])
+
+    return total_cost[-1]
