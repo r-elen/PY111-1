b3b58ed25914ddc5f7724472a396ed7d82fc489e
r-elen <elen.ryazanova@gmail.com>
2022-09-21 17:14:25 +0300

    Practice 3(2) c0 is done

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,14 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib_recursive(n-1) + fib_recursive(n-2)
 
 
 def fib_iterative(n: int) -> int:
@@ -16,8 +22,18 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a = 0
+    b = 1
+    for _ in range(1, n):
+        a, b = b, a + b
+    return b
 
 
 def generator_fib(n):
