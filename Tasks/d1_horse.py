5bd2be43db6aa30abb15114979104dd0f4687a0f
r-elen <elen.ryazanova@gmail.com>
2022-09-23 20:15:53 +0300

    Сделано e0, e1 (bfs, dfs)

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,26 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+
+    rows = shape[0]
+    cols = shape[1]
+
+    def get_step(i, j):
+        if i == 0 and j == 0:  # попали в начальн точку
+            return 1
+        if not 0 <= i < rows:  # левый угол
+            return 0
+        if not 0 <= j <= cols:  # выпали из строки
+            return 0
+
+        return sum([get_step(i-2, j+1),
+                    get_step(i-2, j-1),
+                    get_step(i-1, j-2),
+                    get_step(i+1, j-2)])
+
+    return get_step(i=point[0], j=point[1])
+
+
+if __name__ == '__main__':
+    assert 2 == calculate_paths((4, 4), (3, 3))
+    assert 13309 == calculate_paths((7, 15), (6, 14))
--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,22 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
+import matplotlib.pyplot as plt
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+            )
+
+    plt.show()
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +27,20 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    draw_graph(g)
+    path_node = []
+    visited_nodes = {node: False for node in g.nodes}
+    wait_nodes = deque()
+
+    wait_nodes.append(start_node)
+    while wait_nodes:
+        current_node = wait_nodes.pop()
+        path_node.append(current_node)
+        visited_nodes[current_node] = True  # узел сгорел
+
+        for neighbour in g[current_node]:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)
+                visited_nodes[neighbour] = True
+
+    return path_node
