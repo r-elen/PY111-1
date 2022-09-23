339ba3af6a2e3c6c293e594ac6d1730ca60a22de
r-elen <elen.ryazanova@gmail.com>
2022-09-23 21:27:11 +0300

    Сделано f0 - binary_search

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -3,13 +3,38 @@ You can do it either with networkx ('cause tree is a graph)
 or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 """
 
+# node = {
+#     "key": "",
+#     "value": "",
+#     "left": "",
+#     "right": "",
+#     }
+#
+# root = {
+#     "key": 8,
+#     "left": {
+#         "key": "",
+#         "left": None,
+#         "right": None,
+#         },
+#     "right": "",
+#     }
+
 from typing import Any, Optional, Tuple
 # import networkx as nx
 
 
 class BinarySearchTree:
+    @staticmethod
+    def _create_node(key, value: Any, left: Optional[dict] = None, right: Optional[dict] = None) -> dict:
+        return {"key": key,
+                "value": value,
+                "left": left,
+                "right": right
+                }
+
     def __init__(self):
-        ...
+        self.root: Optional[dict] = None
 
     def insert(self, key: int, value: Any) -> None:
         """
@@ -19,7 +44,25 @@ class BinarySearchTree:
         :param value: value associated with key
         :return: None
         """
-        print(key, value)
+        if self.root is None:
+            self.root = self._create_node(key, value)
+        else:
+            current_node = self.root
+            while True:
+                current_key = current_node["key"]
+                if key > current_key:  # правая часть дерева
+                    right_node = current_node
+                    if right_node is None:
+                        current_node["right"] = self._create_node(key, value)
+                        break
+                    current_node = current_node["right"]
+                else:
+                    left_node = current_node["left"]
+                    if left_node is None:
+                        current_node["left"] = self._create_node(key, value)
+                        break
+                    else:
+                        current_node = current_node["left"]
         return None
 
     def remove(self, key: int) -> Optional[Tuple[int, Any]]:
