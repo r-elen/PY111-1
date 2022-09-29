34092b1b52ba08905930ef00cecb14b4ad1a1b52
r-elen <elen.ryazanova@gmail.com>
2022-09-29 18:48:51 +0300

    g0 - добавлено условие, когда список уже отсортирован

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,10 +10,14 @@ def sort(container: List[int]) -> List[int]:
     """
     offset = 1
     for _ in range(len(container)):
+        is_change = False  # список изначально отсортирован
         for i in range(len(container) - offset):
             current_elem = container[i]
             next_elem = container[i+1]
             if current_elem > next_elem:
                 container[i], container[i+1] = next_elem, current_elem
+                is_change = True
+        if not is_change:
+            break
         offset += 1
     return container
