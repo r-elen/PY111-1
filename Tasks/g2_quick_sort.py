3aed2953ce6ed90739584b2b4f1e5c5da6420039
r-elen <elen.ryazanova@gmail.com>
2022-09-29 21:11:41 +0300

    Сделано - g2 - quick sort

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,4 +1,5 @@
 from typing import List
+from random import randint, choice
 
 
 def sort(container: List[int]) -> List[int]:
@@ -8,4 +9,50 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    if not container:
+        return container
+    pivot = choice(container)
+    return sort([v for v in container if v < pivot])\
+        + [v for v in container if v == pivot]\
+        + sort([v for v in container if v > pivot])
+
+
+    # первый вариант
+    # def _sort(left_border, right_border):
+    #     if left_border >= right_border:
+    #         return
+    #     random_index = randint(left_border, right_border)
+    #     pivot = container[random_index]
+    #
+    #     i, j = left_border, right_border
+    #     while i <= j:
+    #         while container[i] < pivot:
+    #             i += 1  # увеличивает на 1 пока не встретит равный или больше опорного
+    #         while container[j] > pivot:
+    #             j -= 1  # уменьшаем на 1 пока не встретит равный или меньше опорного
+    #         if i <= j:
+    #             container[i], container[j] = container[j], container[i]
+    #             i += 1
+    #             j -= 1
+    #     _sort(left_border, j)
+    #     _sort(right_border, i)
+    #
+    # _sort(0, len(container))
+    #
+    # return container
+
+
+    # # мои идеи (не доделано)
+    # random_index = randint(left_border, right_border)
+    # support_elem = container[random_index]
+    # left_result = []
+    # right_result = []
+    #
+    # for _ in container:
+    #     for i in range(len(container)):
+    #         if container[i] < support_elem:
+    #             left_result.append(container[i])
+    #         if container[i] > support_elem:
+    #             right_result.append(container[i])
+    # return container
