a555cb07652d9f6ff7dfb9c6db63737a19435955
r-elen <elen.ryazanova@gmail.com>
2022-09-29 18:41:49 +0300

    Сделано g0

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,11 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+
+    for _ in range(len(container)):
+        for i in range(len(container)-1):
+            current_elem = container[i]
+            next_elem = container[i+1]
+            if current_elem > next_elem:
+                container[i], container[i+1] = next_elem, current_elem
     return container
