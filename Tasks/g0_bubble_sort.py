cb74b3cf064f1c9b6c0c42a117515210e140a4d2
r-elen <elen.ryazanova@gmail.com>
2022-09-29 18:44:19 +0300

    g0 - add offset

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,11 +8,12 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-
+    offset = 1
     for _ in range(len(container)):
-        for i in range(len(container)-1):
+        for i in range(len(container) - offset):
             current_elem = container[i]
             next_elem = container[i+1]
             if current_elem > next_elem:
                 container[i], container[i+1] = next_elem, current_elem
+        offset += 1
     return container
